240a3d07633f0a226f8a23ae9258015ea0c12e15
swilt1onu <112091288+swilt1onu@users.noreply.github.com>
2023-03-28 10:07:11 -0400

    added the store 1 items

---
+++ OregonTrail/app/src/main/java/com/example/oregontrail/Items.java
@@ -0,0 +1,21 @@
+package com.example.oregontrail;
+import java.util.ArrayList;
+
+public class Items {
+
+    //constructor
+    public Items() {}
+
+
+    //array list for the items in the first shop
+    String[] store1Items = {"0_Food_2000pounds_$.05/lb","1_Clothing_8sets_$free",
+            "2_CookingItems_20_$1each","3_Oxen_6_$30each","4_SpareWagonWheels_3_$8each",
+            "5_Medkit_1_$3", "6_Blankets_6_$.50each", "7_SewingKit_1_$2",
+            "8_ExtraWagonTarp_2_$3each","9_FireStartingKit_4_$.30each"};
+
+
+
+
+
+
+}
