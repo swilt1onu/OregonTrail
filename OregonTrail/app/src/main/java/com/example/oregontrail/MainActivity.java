67ebd1c00794878e85daa62c04c6556961e2c891
swilt1onu <112091288+swilt1onu@users.noreply.github.com>
2023-04-02 13:48:31 -0400

    Changed the boxes to be able to be edited

--- OregonTrail/app/src/main/java/com/example/oregontrail/MainActivity.java
+++ OregonTrail/app/src/main/java/com/example/oregontrail/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.oregontrail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-
 
 
 
@@ -21,6 +21,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //boxes listed from top down
+        final TextView OregonTrailTitleLabel = findViewById(R.id.OregonTrailTitleLabel);
+
+        final EditText userOutputBox = findViewById(R.id.userOutputBox);
+
+        final EditText userOutputStatsBox = findViewById(R.id.userOutputStatsBox);
+
+        final EditText userInputBox = findViewById(R.id.userInputBox);
+
+
+
+
+
+        
     }
 }
 
