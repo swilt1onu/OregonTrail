2b0188ac631ba9e36776dfed8511c5a94a759f7c
h-charles <h-charles@onu.edu>
2023-04-02 21:41:25 -0400

    Added weather and temperature

--- OregonTrail/app/src/main/java/com/example/oregontrail/Trail.java
+++ OregonTrail/app/src/main/java/com/example/oregontrail/Trail.java
@@ -12,10 +12,17 @@ public class Trail {
     Random generator = new Random(System.currentTimeMillis());
     //Variables
     private int landmarkIndex = 0;
-    private int currentWeather = 0;
-    private int region = 0;
-    private int location = 0;
+    private String currentWeather = "Sunny";
+    private int distance = 0;
     private int randomEventIndex = 0;
+
+    private int dayCount = 0;
+
+    private int month = 0; //Set at the beginning
+
+    private int year = 0; //Set by player
+
+    private int totalDistance = 0; //Ideally set to the total distance of the trip
 
     //methods
     public void randomEvent (int randomEventIndex){
@@ -23,27 +30,28 @@ public class Trail {
         //lists and different events to be used by the index
     }
 
-    public int getCurrentWeather() {
+    public String getCurrentWeather() {
         int temp;
         temp = generator.nextInt(100);
-        if (region == 1){
-            System.out.println("Placeholder, temp value would be " + temp);
+
+        if ((temp % 20) == 0) {
+            if (month > 10 || month < 4) {
+                currentWeather = "Snowing";
+            } else {
+                currentWeather = "Raining";
+            }
         }
-        if (region == 2){
-            System.out.println("Placeholder, temp value would be " + temp);
+        if (temp == 0) {
+            if (month > 10 || month < 4) {
+                currentWeather = "Blizzard";
+            } else {
+                currentWeather = "Storm";
+            }
         }
-        if (region == 3){
-            System.out.println("Placeholder, temp value would be " + temp);
-        }
-        if (region == 4){
-            System.out.println("Placeholder, temp value would be " + temp);
-        }
-        if (region == 5){
-            System.out.println("Placeholder, temp value would be " + temp);
-        }
-        if (region == 6){
-            System.out.println("Placeholder, temp value would be " + temp);
-        }
+        return currentWeather;
+    }
+
+    public String getWeather() {
         return currentWeather;
     }
 
@@ -52,19 +60,20 @@ public class Trail {
         return landmarkIndex;
     }
 
-    public int getRegion() {
-        return region;
+
+    public int getDistance() {
+        return distance;
     }
 
-    public int getLocation() {
-        return location;
+    public int getTotalDistance() {
+        return totalDistance;
     }
 
     public int getRandomEventIndex() {
         return randomEventIndex;
     }
 
-    public void setCurrentWeather(int currentWeather) {
+    public void setCurrentWeather(String currentWeather) {
         this.currentWeather = currentWeather;
     }
 
@@ -72,18 +81,69 @@ public class Trail {
         this.landmarkIndex = landmarkIndex;
     }
 
-    public void setLocation(int location) {
-        this.location = location;
+    public void setDistance(int pace) {
+        if (pace == 3) {this.distance = this.distance + 15;}
+        if (pace == 2) {this.distance = this.distance + 10;}
+        if (pace == 1) {this.distance = this.distance + 5;}
+        if (pace == 0) {this.distance = this.distance + 0;}
     }
 
     public void setRandomEventIndex(int randomEventIndex) {
         this.randomEventIndex = randomEventIndex;
     }
 
-    public void setRegion(int region) {
-        if (region > 6 || region < 1){
-            System.out.println("Error: Region was set to invalid value.");
+    public int getTemperature () {
+        int temperature = 0;
+        int temp1 = 0;
+        int temp2 = 0;
+        if (month > 11 && month < 3) {
+            temperature = 20;
+            temp1 = generator.nextInt(15);
+            temp2 = generator.nextInt(2);
+            if ((temp2 % 2) == 0) {
+                temperature -= temp1;
+            } else {
+                temperature += temp1;
+            }
         }
-        else {this.region = region;}
+        if (month > 2 && month < 6) {
+            temperature = 50;
+            temp1 = generator.nextInt(15);
+            temp2 = generator.nextInt(2);
+            if ((temp2 % 2) == 0) {
+                temperature -= temp1;
+            } else {
+                temperature += temp1;
+            }
+        }
+        if (month > 5 && month < 9) {
+            temperature = 70;
+            temp1 = generator.nextInt(15);
+            temp2 = generator.nextInt(2);
+            if ((temp2 % 2) == 0) {
+                temperature -= temp1;
+            } else {
+                temperature += temp1;
+            }
+        }
+        if (month > 8 && month < 12) {
+            temperature = 40;
+            temp1 = generator.nextInt(15);
+            temp2 = generator.nextInt(2);
+            if ((temp2 % 2) == 0) {
+                temperature -= temp1;
+            } else {
+                temperature += temp1;
+            }
+        }
+        return temperature;
+    }
+
+    public void Day () {
+        //This method should simulate a day
+        //Rolls for weather, random events, temperature
+        //Food consumption and distance is updated
+        //Increments day and sets months/years accordingly to day count
+
     }
 }
